017d77501021e55a09d948f61447ecd3d486e6b9
Dídac Fernández Cadenas <d1dac_94@hotmail.com>
2016-02-04 17:26:01 +0100

    Scripts for the player
    Controlling the player and interacting with collisions. Starting
    classes.

---
+++ Intro Game/Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController2D : MonoBehaviour {
+
+	private const float SkinWidth = .02f;
+	private const int TotalHorizontalRays = 8;
+	private const int TotalVerticalRays = 4;
+
+	private static readonly float SlopeLimitTangent = Mathf.Tan(75f * Mathf.Deg2Rad);
+
+	public LayerMask PlatformMask;
+	public ControllerParameters2D DefaultParameters;
+
+	public ControllerState2D State { get; private set;}
+
+	public void Awake() {
+
+	}
+
+	public void AddForce (Vector2 force) {
+
+	}
+
+	public void SetForce (Vector2 force) {
+
+	}
+
+	public void SetHorizontalForce (float x) {
+
+	}
+
+	public void SetVerticalForce (float x) {
+
+	}
+
+	public void Jump() {
+
+	}
+
+	public void LateUpdate() {
+
+	}
+
+	private void HandlePlatforms () {
+
+	}
+
+	private void CalculateRayOrigins () {
+
+	}
+
+	private void MoveHorizontally (ref Vector2 deltaMovement) {
+
+	}
+
+
+}
