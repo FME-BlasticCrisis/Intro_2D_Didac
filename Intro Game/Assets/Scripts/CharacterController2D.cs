85e333b08b34167a8dd8486bb6d814399ba70ed2
Dídac Fernández Cadenas <d1dac_94@hotmail.com>
2016-02-13 17:52:43 +0100

    Slope Handling
    Added slope handling. Two fixes marked with comments may be removed in
    the future

--- Intro Game/Assets/Scripts/CharacterController2D.cs
+++ Intro Game/Assets/Scripts/CharacterController2D.cs
@@ -212,11 +212,53 @@ public class CharacterController2D : MonoBehaviour {
 	}
 
 	private void HandleVerticalSlope (ref Vector2 deltaMovement) {
+		var center = (_rayCastBottomLeft.x + _rayCastBottomRight.x) / 2;
+		var direction = -Vector2.up;
 
+		var slopeDistance = SlopeLimitTangent * (_rayCastBottomRight.x - center);
+		var slopeRayVector = new Vector2 (center, _rayCastBottomLeft.y);
+
+		Debug.DrawRay (slopeRayVector, direction * slopeDistance, Color.yellow);
+
+		var rayCastHit = Physics2D.Raycast (slopeRayVector, direction, slopeDistance, PlatformMask);
+		if (!rayCastHit)
+			return;
+
+		// ReSharper disable CompareOfFloatsByEqualityOperator
+
+		var isMovingDownSlope = Mathf.Sign (rayCastHit.normal.x) == Mathf.Sign (deltaMovement.x);
+		if (!isMovingDownSlope)
+			return;
+
+		var angle = Vector2.Angle (rayCastHit.normal, Vector2.up);
+		if (Mathf.Abs (angle) < .0001f)
+			return;
+
+		State.IsMovingDownSlope = true;
+		State.SlopeAngle = angle;
+		deltaMovement.y = rayCastHit.point.y - slopeRayVector.y;
 	}
 
 	private bool HandleHorizontalSlope (ref Vector2 deltaMovement, float angle, bool IsGoingRight) {
-		return false;
+		if (Mathf.RoundToInt (angle) == 90)
+			return false;
+
+		if (angle > Parameters.SlopeLimit) {
+			deltaMovement.x = 0;
+			return true;
+		}
+
+		if (deltaMovement.y > .07f)
+			return true;
+
+		deltaMovement.x += IsGoingRight ? -SkinWidth : SkinWidth;
+		// I multiply the vertical speed by a constant because it is too slow for the time being :(
+		deltaMovement.y = 3 * Mathf.Abs (Mathf.Tan (angle * Mathf.Deg2Rad) * deltaMovement.x);
+		// Also tweking horizontal speed to see if it helps :P
+		deltaMovement.x += IsGoingRight ? .01f : -.01f;
+		State.IsMovingUpSlope = true;
+		State.IsCollidingRight = true;
+		return true;
 	}
 
 	public void OnTriggerEnter2D (Collider2D other) {
