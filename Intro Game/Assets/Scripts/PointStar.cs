76c95b2ff9fe2ab4ce90dfe18a58f7de2e4d1232
Dídac Fernández Cadenas <d1dac_94@hotmail.com>
2018-10-21 18:56:30 +0200

    meta update

--- Intro Game/Assets/Scripts/PointStar.cs
+++ Intro Game/Assets/Scripts/PointStar.cs
@@ -6,8 +6,14 @@ public class PointStar : MonoBehaviour, IPlayerRespawnListener {
 	public GameObject Effect;
 	public int PointsToAdd = 10;
 	public AudioClip HitStarSound;
+	public Animator Animator;
+
+	private bool _isCollected;
 
 	public void OnTriggerEnter2D(Collider2D other) {
+		if (_isCollected)
+			return;
+
 		if (other.GetComponent<Player> () == null)
 			return;
 
@@ -16,13 +22,19 @@ public class PointStar : MonoBehaviour, IPlayerRespawnListener {
 
 		GameManager.Instance.AddPoints (PointsToAdd);
 		Instantiate (Effect, transform.position, transform.rotation);
-
-		gameObject.SetActive (false);
-
 		FloatingText.Show (string.Format ("+{0}!", PointsToAdd), "PointStarText", new FromWorldPointTextPositioner (Camera.main, transform.position, 1.5f, 50f));
+
+		_isCollected = true;
+
+		Animator.SetTrigger ("Collect");
+	}
+
+	public void FinishAnimationEvent() {
+		gameObject.SetActive (false);
 	}
 
 	public void OnPlayerRespawnInThisCheckpoint(Checkpoint checkpoint, Player player) {
+		_isCollected = false;
 		gameObject.SetActive(true);
 	}
 }
